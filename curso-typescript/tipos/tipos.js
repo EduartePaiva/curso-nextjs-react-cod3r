69bc35600718fb8e7217f41cc9fad244b6241161
EduartePaiva <eduarte2018.po@gmail.com>
2023-04-04 12:28:46 -0300

    estudo ts
    ts

---
+++ curso-typescript/tipos/tipos.js
@@ -0,0 +1,18 @@
+"use strict";
+// string
+let nome = "João";
+console.log(nome);
+//nome = 28
+//numbers
+let idade = 27;
+idade = 27.5;
+console.log(idade);
+//boolean
+let possuiHobbies = false;
+//possuiHobbies = 1
+console.log(possuiHobbies);
+//tipos explicitos
+let minhaIdade;
+minhaIdade = 27;
+console.log(typeof minhaIdade);
+//minhaIdade = 'idade é 27'
---
+++ curso-typescript/tipos/tipos.ts
@@ -0,0 +1,22 @@
+// string
+let nome: string = "João"
+console.log(nome)
+
+//nome = 28
+
+//numbers
+let idade: number = 27
+idade = 27.5
+console.log(idade)
+
+//boolean
+let possuiHobbies: boolean = false
+//possuiHobbies = 1
+console.log(possuiHobbies)
+
+
+//tipos explicitos
+let minhaIdade: number
+minhaIdade = 27
+console.log(typeof minhaIdade)
+//minhaIdade = 'idade é 27'
