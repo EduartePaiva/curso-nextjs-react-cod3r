3bec135e4d5c14c3817c760b24bc2da0abd1cfb7
EduartePaiva <eduarte2018.po@gmail.com>
2023-04-10 15:36:26 -0300

    começo do curso
    revisão react

---
+++ exercicios/src/pages/basicos/jsx2.tsx
@@ -0,0 +1,17 @@
+export default function jsx2() {
+    const array = [
+        <li key={1}>Paulo</li>,
+        <li key={2}>Ana</li>,
+        <li key={3}>Pedro</li>
+    ]
+    const conteudo = (
+        <div>
+            <h1>JSX #02</h1>
+        </div>
+    )
+    return conteudo
+}
+
+// HTML, CSS e JS - Tecnologias
+
+// Componentes (HTML, JS, CSS)
